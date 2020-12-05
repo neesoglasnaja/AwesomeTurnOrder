1fbe6773eafb5314020bc5dd0029022914b1f478
Sasha Shinkevich <neesa.web@gmail.com>
2020-12-06 01:23:24 +0300

    Remove jQuery methods and checks

--- content.js
+++ content.js
@@ -6,7 +6,7 @@
             "hp": "@{hp}",
             "pp": "@{passive_wisdom}",
         };
-        const initiativeTemplate = `<script id="tmpl_initiativecharacter" type="text/html">
+        const initiativeTemplate = `
         <![CDATA[
             <li class='__awesome-turn-order--item token <$ if (this.layer === "gmlayer") { $>gmlayer<$ } $>' data-tokenid='<$!this.id$>' data-currentindex='<$!this.idx$>'>
                 <$ var token = Campaign.pages.get(Campaign.activePage()).thegraphics.get(this.id); $>
@@ -17,7 +17,7 @@
                 <$ if (this.avatar) { $><img class='avatar' src='<$!this.avatar$>' /><$ } $>
                 <span class='name'><$!this.name$></span>
                 <div class="__awesome-turn-order--extra">
-                    <!--5ETOOLS_REPLACE_TARGET-->
+                    <!--REPLACE_TARGET-->
                 </div>
                 <span alt='Initiative' title='Initiative' class='initiative <$ if (this.iseditable) { $>editable<$ } $>'>
                     <$!this.pr$>
@@ -26,8 +26,7 @@
                     <span class='pictos remove'>#</span>
                 </div>
             </li>
-        ]]>
-        </script>`;
+        ]]>`;
 
         const tokenBindings = {};
         // Bind tokens to the initiative tracker
@@ -39,10 +38,10 @@
         // bind token HP to initiative tracker window HP field
         const bindToken = function (token) {
             function getInitTrackerToken() {
-                const $window = $("#initiativewindow");
-                if (!$window.length) return [];
-                return $window.find(`li.token`).filter((i, e) => {
-                    return $(e).data("tokenid") === token.id;
+                const $initiativeContainer = document.querySelector("#initiativewindow");
+                if (!$initiativeContainer.length) return [];
+                return [...$initiativeContainer.querySelectorAll(`li.token`)].find(($element) => {
+                    return $element.getAttribute('[data-tokenid]') === token.id;
                 });
             }
 
@@ -55,22 +54,22 @@
             const toBind = (token, changes) => {
                 const $initToken = getInitTrackerToken();
                 if (!$initToken.length) return;
-                const $iptHp = $initToken.find(`.hp`);
+                const $iptHp = $initToken.querySelector(`.hp`);
 
                 if (!token.character || npcFlag && npcFlag.get("current") == "1") {
                     if (changes.changes[`bar1_value`]) {
-                        $iptHp.text(token.changed[`bar1_value`]);
+                        $iptHp.innerText = token.changed[`bar1_value`];
                     }
                 } else {
-                    $iptHp.text(token.character.autoCalcFormula(formulas.hp));
+                    $iptHp.innerText = token.character.autoCalcFormula(formulas.hp);
                 }
 
             };
             // clean up old handler
-            if (tokenBindings[token.id]) token.off("change", tokenBindings[token.id]);
+            if (tokenBindings[token.id]) token.removeEventListener("change", tokenBindings[token.id]);
             // add new handler
             tokenBindings[token.id] = toBind;
-            token.on("change", toBind);
+            token.addEventListener("change", toBind);
         };
 
         const initTurnOrderPatch = function () {
@@ -78,19 +77,30 @@
 
             const $initiativeContent = document.querySelector('#initiativewindow')
             $initiativeContent.style.setProperty('height', '350px');
-            
+
             const $initiativeDialog = $initiativeContent.closest('[role="dialog"]');
             $initiativeDialog.classList.add('__awesome-turn-order');
             $initiativeDialog.style.setProperty('width', '350px');
 
             Campaign.initiativewindow.rebuildInitiativeList = function () {
                 let html = initiativeTemplate;
-                $(".tracker-header-extra-columns").empty();
+                const $extraContainer = document.querySelector(".__awesome-turn-order--extra");
+                if ($extraContainer) {
+                    $extraContainer.innerHTML = '';
+                }
+
 
                 const replaceStack = [
                     `
-                        <$ var passive = (typeof char !== "undefined" && char && typeof char.autoCalcFormula !== "undefined") ? (char.autoCalcFormula('@{passive_wisdom}') || char.autoCalcFormula('${formulas.pp}')) : "\u2014"; $>
-                        <span class='pp __awesome-turn-order--extra-prop' alt='Passive Perception' title='Perc'><$!passive$></span>
+                        <span class='hp __awesome-turn-order--extra-prop' alt='HP' title='HP'>
+                            <$ if(npc && npc.get("current") == "1") { $>
+                                <$!token.attributes.bar1_value$>
+                            <$ } else if (typeof char !== "undefined" && char && typeof char.autoCalcFormula !== "undefined") { $>
+                                <$!char.autoCalcFormula('${formulas.hp}')$>
+                            <$ } else { $>
+                                <$!"\u2014"$>
+                            <$ } $>
+                        </span>
                     `,
                     `
                         <span class='ac __awesome-turn-order--extra-prop' alt='AC' title='AC'>
@@ -104,20 +114,14 @@
                         </span>
                     `,
                     `
-                    <span class='hp __awesome-turn-order--extra-prop' alt='HP' title='HP'>
-                        <$ if(npc && npc.get("current") == "1") { $>
-                            <$!token.attributes.bar1_value$>
-                        <$ } else if (typeof char !== "undefined" && char && typeof char.autoCalcFormula !== "undefined") { $>
-                            <$!char.autoCalcFormula('${formulas.hp}')$>
-                        <$ } else { $>
-                            <$!"\u2014"$>
-                        <$ } $>
-                    </span>
-                `,
+                        <$ var passive = (typeof char !== "undefined" && char && typeof char.autoCalcFormula !== "undefined") ? (char.autoCalcFormula('@{passive_wisdom}') || char.autoCalcFormula('${formulas.pp}')) : "\u2014"; $>
+                        <span class='pp __awesome-turn-order--extra-prop' alt='Passive Perception' title='Perc'><$!passive$></span>
+                    `,
                 ];
 
-                html = html.replace(`<!--5ETOOLS_REPLACE_TARGET-->`, replaceStack.reverse().join(" \n"));
-                $("#tmpl_initiativecharacter").replaceWith(html);
+                html = html.replace(`<!--REPLACE_TARGET-->`, replaceStack.join(" \n"));
+                document.querySelector("#tmpl_initiativecharacter").innerHTML = html;
+
 
                 const results = turnOrderCachedFunction.apply(this, []);
                 setTimeout(function () {
@@ -132,7 +136,7 @@
         // if we are the topmost frame, inject
         if (window.top === window.self) {
             (function doCheckDepsLoaded() {
-                if (document.readyState === 'complete' && typeof window.$ !== "undefined" && window.Campaign && window.Campaign.initiativewindow) {
+                if (document.readyState === 'complete' && window.Campaign && window.Campaign.initiativewindow) {
                     initTurnOrderPatch();
                 } else {
                     setTimeout(doCheckDepsLoaded, 500);
